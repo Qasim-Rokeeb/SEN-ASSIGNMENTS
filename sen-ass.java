3ecb91f818edfe07679c5ad6000652981f793b61
Qasim Rokeeb <qasimrokeeb@gmail.com>
2025-09-22 17:02:56 +0100

    removed unused comment

--- sen-ass.java
+++ sen-ass.java
@@ -14,13 +14,7 @@ class Factorial {
         if (n == 0 || n == 1) return 1;
         return n * factorialRecursive(n - 1);
     }
-    // class Factorial {
-    // // Recursive
-    // public static long factorialRecursive(int n) {
-    //     if (n == 0 || n == 1) return 1;
-    //     return n * factorialRecursive(n - 1);
-    // }
-
+   
 
     // Iterative
     public static long factorialIterative(int n) {
